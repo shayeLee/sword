f68a2fe4fc214865e93ee067c9157d1b136260f4
lxy <lxyuid@126.com>
2018-11-26 18:00:49 +0800

    modify export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-export crypto from "./crypto";
-export dateTool from "./dateTool";
-export variables from "./variables";
-export arrTool from "./arrTool";
-export strTool from "./strTool";
-export objTool from "./objTool";
-export fileTool from "./fileTool";
+import _crypto from "./crypto";
+export const crypto = _crypto;
+
+import _dateTool from "./dateTool";
+export const dateTool = _dateTool;
+
+import _variables from "./variables";
+export const variables = _variables;
+
+import _arrTool from "./arrTool";
+export const arrTool = _arrTool;
+
+import _strTool from "./strTool";
+export const strTool = _strTool;
+
+import _objTool from "./objTool";
+export const objTool = _objTool;
+
+import _fileTool from "./fileTool";
+export const fileTool = _fileTool;
